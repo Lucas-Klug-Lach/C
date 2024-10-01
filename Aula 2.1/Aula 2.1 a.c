e84818cb583e65071d0fd24fae6f6cb82bc93213
Lucas Klug Lach <167248764+Lucas-Klug-Lach@users.noreply.github.com>
2024-10-01 20:25:27 -0300

    Update

---
+++ Aula 2.1/Aula 2.1 a.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+	
+	printf("Oi, tudo bem? Tenho 11 anos e programo.\n \a");
+	
+	printf("Valor inteiro: %d. \n", 10);
+	printf("Valor real: %f. \n", 3.14159265);
+	printf("Valor real com apenas duas casas: %. 2f .\n", 3.14159265);
+	printf("Dado de texto: %c. \n", 'a');
+	printf("Dado de texto: %s. \n", "Testando");
+	
+	system("pause");
+	
+}
