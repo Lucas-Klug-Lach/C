5c796bc026ed8450cebf012c8e2c9a9331664007
Lucas Klug Lach <167248764+Lucas-Klug-Lach@users.noreply.github.com>
2024-10-28 11:07:21 -0300

    Update

---
+++ Exercicios/Aula 4.1/Aula 4.1.C
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+	
+	float m;
+	
+	printf("Insira a nota: \n");
+	scanf("%f", &m);
+	
+	if(m >= 7.0){
+		printf("Aprovado :D. \n \a");
+	}
+	
+	
+	system("pause");
+}
