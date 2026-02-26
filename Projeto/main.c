aa5ec59e17824b7161e5994b18d0171f6c6c111f
Lucas Klug Lach <167248764+Lucas-Klug-Lach@users.noreply.github.com>
2026-02-26 17:22:45 -0300

    up

---
+++ Projeto/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include "poligonos.h"
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	
+	printf("%.2f.\n", calcAreaRetangulo(10,10));
+	return 0;
+}
